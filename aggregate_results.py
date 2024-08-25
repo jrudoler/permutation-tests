d8986b9d5a4fd4db75e6cba1e2740df476729e56
Joey Rudoler <jhrudoler@gmail.com>
2024-08-25 19:29:32 -0400

    new data viz, fixed geq vs leq for scores where higher score is better/worse

--- aggregate_results.py
+++ aggregate_results.py
@@ -58,19 +58,38 @@ maha_values = np.linspace(0.0, 1.5, 5)
 #     for p in param_range:
 #         for i in range(nsim):
 #             try:
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"post_testsize_{p:.4f}_simno_{i:05}.pkl"))
-#                 testsize_post = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"pre_testsize_{p:.4f}_simno_{i:05}.pkl"))
-#                 testsize_pre = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 testsize_pre['test'] = "pre"; testsize_post['test'] = "post"
-#                 testsize_pre['d'] = m; testsize_post['d'] = m
-#                 testsize_pre['param'] = p; testsize_post['param'] = p
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"post_testsize_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 testsize_post = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"pre_testsize_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 testsize_pre = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 testsize_pre["test"] = "pre"
+#                 testsize_post["test"] = "post"
+#                 testsize_pre["d"] = m
+#                 testsize_post["d"] = m
+#                 testsize_pre["param"] = p
+#                 testsize_post["param"] = p
 #                 testsize = pd.concat([testsize, testsize_pre, testsize_post])
-#             except FileNotFoundError: # some errors when only one class is present
+#             except FileNotFoundError:  # some errors when only one class is present
 #                 print(f"File not found: param={p}, simno={i}, maha={m}")
-# testsize = testsize.reset_index(names = "simno")
+# testsize = testsize.reset_index(names="simno")
 # testsize.to_pickle("testsize.pkl")
 
 # del testsize
@@ -86,19 +105,42 @@ maha_values = np.linspace(0.0, 1.5, 5)
 #     for p in param_range:
 #         for i in range(nsim):
 #             try:
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"post_samplesize_{p:.4f}_simno_{i:05}.pkl"))
-#                 samplesize_post = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"pre_samplesize_{p:.4f}_simno_{i:05}.pkl"))
-#                 samplesize_pre = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 samplesize_pre['test'] = "pre"; samplesize_post['test'] = "post"
-#                 samplesize_pre['d'] = m; samplesize_post['d'] = m
-#                 samplesize_pre['param'] = p; samplesize_post['param'] = p
+#                 score, null = pd.read_pickle(
+#                     os.path.join(
+#                         results_dir, f"post_samplesize_{p:.4f}_simno_{i:05}.pkl"
+#                     )
+#                 )
+#                 samplesize_post = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 score, null = pd.read_pickle(
+#                     os.path.join(
+#                         results_dir, f"pre_samplesize_{p:.4f}_simno_{i:05}.pkl"
+#                     )
+#                 )
+#                 samplesize_pre = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 samplesize_pre["test"] = "pre"
+#                 samplesize_post["test"] = "post"
+#                 samplesize_pre["d"] = m
+#                 samplesize_post["d"] = m
+#                 samplesize_pre["param"] = p
+#                 samplesize_post["param"] = p
 #                 samplesize = pd.concat([samplesize, samplesize_pre, samplesize_post])
-#             except FileNotFoundError: # some errors when only one class is present
+#             except FileNotFoundError:  # some errors when only one class is present
 #                 print(f"File not found: param={p}, simno={i}, maha={m}")
-# samplesize = samplesize.reset_index(names = "simno")
+# samplesize = samplesize.reset_index(names="simno")
 # samplesize.to_pickle("samplesize.pkl")
 
 # del samplesize
@@ -114,19 +156,38 @@ maha_values = np.linspace(0.0, 1.5, 5)
 #     for p in param_range:
 #         for i in range(nsim):
 #             try:
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"post_nfeats_{p:.4f}_simno_{i:05}.pkl"))
-#                 nfeats_post = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"pre_nfeats_{p:.4f}_simno_{i:05}.pkl"))
-#                 nfeats_pre = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 nfeats_pre['test'] = "pre"; nfeats_post['test'] = "post"
-#                 nfeats_pre['d'] = m; nfeats_post['d'] = m
-#                 nfeats_pre['param'] = p; nfeats_post['param'] = p
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"post_nfeats_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 nfeats_post = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"pre_nfeats_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 nfeats_pre = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 nfeats_pre["test"] = "pre"
+#                 nfeats_post["test"] = "post"
+#                 nfeats_pre["d"] = m
+#                 nfeats_post["d"] = m
+#                 nfeats_pre["param"] = p
+#                 nfeats_post["param"] = p
 #                 nfeats = pd.concat([nfeats, nfeats_pre, nfeats_post])
-#             except FileNotFoundError: # some errors when only one class is present
+#             except FileNotFoundError:  # some errors when only one class is present
 #                 print(f"File not found: param={p}, simno={i}, maha={m}")
-# nfeats = nfeats.reset_index(names = "simno")
+# nfeats = nfeats.reset_index(names="simno")
 # nfeats.to_pickle("nfeats.pkl")
 
 # del nfeats
@@ -142,19 +203,38 @@ maha_values = np.linspace(0.0, 1.5, 5)
 #     for p in param_range:
 #         for i in range(nsim):
 #             try:
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"post_ratio_{p:.4f}_simno_{i:05}.pkl"))
-#                 ratio_post = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 score, null = pd.read_pickle(os.path.join(results_dir, f"pre_ratio_{p:.4f}_simno_{i:05}.pkl"))
-#                 ratio_pre = pd.merge(pd.DataFrame(score, index=[i]),
-#                                         pd.DataFrame(null, i+np.zeros(len(null)).astype(int)).add_prefix("null_"), left_index=True, right_index=True)
-#                 ratio_pre['test'] = "pre"; ratio_post['test'] = "post"
-#                 ratio_pre['d'] = m; ratio_post['d'] = m
-#                 ratio_pre['param'] = p; ratio_post['param'] = p
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"post_ratio_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 ratio_post = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 score, null = pd.read_pickle(
+#                     os.path.join(results_dir, f"pre_ratio_{p:.4f}_simno_{i:05}.pkl")
+#                 )
+#                 ratio_pre = pd.merge(
+#                     pd.DataFrame(score, index=[i]),
+#                     pd.DataFrame(null, i + np.zeros(len(null)).astype(int)).add_prefix(
+#                         "null_"
+#                     ),
+#                     left_index=True,
+#                     right_index=True,
+#                 )
+#                 ratio_pre["test"] = "pre"
+#                 ratio_post["test"] = "post"
+#                 ratio_pre["d"] = m
+#                 ratio_post["d"] = m
+#                 ratio_pre["param"] = p
+#                 ratio_post["param"] = p
 #                 ratio = pd.concat([ratio, ratio_pre, ratio_post])
-#             except FileNotFoundError: # some errors when only one class is present
+#             except FileNotFoundError:  # some errors when only one class is present
 #                 print(f"File not found: param={p}, simno={i}, maha={m}")
-# ratio = ratio.reset_index(names = "simno")
+# ratio = ratio.reset_index(names="simno")
 # ratio.to_pickle("ratio.pkl")
 
 for parameter in ["testsize", "samplesize", "nfeats", "ratio"]:
@@ -163,9 +243,17 @@ for parameter in ["testsize", "samplesize", "nfeats", "ratio"]:
     long_data["param"] = long_data["param"].round(3)
     for metric in ["roc_auc", "accuracy", "brier_score", "log_loss"]:
         print(f"Evaluating {metric}")
-        long_data["null_exceeds_score"] = (
-            long_data[f"null_{metric}"] >= long_data[metric]
-        ).astype(int)
+        if metric in ["brier_score", "log_loss"]:
+            # lower is better
+            long_data["null_exceeds_score"] = (
+                long_data[f"null_{metric}"] <= long_data[metric]
+            ).astype(int)
+        else:
+            # higher is better
+            long_data["null_exceeds_score"] = (
+                long_data[f"null_{metric}"] >= long_data[metric]
+            ).astype(int)
+
         agg_data = (
             long_data.groupby(["d", "param", "test", "simno", metric])
             .agg({"null_exceeds_score": lambda x: (x.sum() + 1) / (len(x) + 1)})
